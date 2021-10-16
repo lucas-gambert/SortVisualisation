79a2a4563e59d63b4944b7881103ac12a16cb6d1
lgambert <lgambert91310@gmail.com>
2021-10-16 23:48:25 +0200

    added bubble, fusion and quick sorting algo

--- src/app/components/show-tab/show-tab.component.ts
+++ src/app/components/show-tab/show-tab.component.ts
@@ -29,7 +29,7 @@ export class ShowTabComponent implements OnInit {
     }
   }
 
-  //Mon propre algo de tri basique pour comparer
+  //Mon propre algo de tri basique pour comparer (par insertion ?)
   public sort(tab) {
     let start = performance.now();
     console.log(start);
@@ -43,10 +43,118 @@ export class ShowTabComponent implements OnInit {
           tab[j] = smallest;
           tab[i] = tmp;
           smallest = tmp;
+
+          this.tab = tab;
         }
       }
     }
     this.tab = tab;
+    let end = performance.now();
+    console.log(end);
+    this.time = end - start;
+  }
+
+  //Algorithme de tri à bulles
+  public sortBubble(tab) {
+    let start = performance.now();
+    console.log(start);
+
+    let isSwapped
+    do {
+      isSwapped = false;
+      for (let i = 0; i < tab.length; i++) {
+        setTimeout(() => { }, 100000);
+        if (tab[i] > tab[i + 1]) {
+          let tmpLeft = tab[i];
+          tab[i] = tab[i + 1];
+          tab[i + 1] = tmpLeft;
+          isSwapped = true;
+        }
+      }
+    } while (isSwapped);
+
+    let end = performance.now();
+    console.log(end);
+    this.time = end - start;
+  }
+
+  //Algorithme de tri fusion
+  public sortFusion(tab) {
+    if (tab.length > 1) {
+      let middleIndex = Math.floor(tab.length / 2)
+      let leftSide = tab.slice(0, middleIndex)
+      let rightSide = tab.slice(middleIndex)
+      this.sortFusion(leftSide)
+      this.sortFusion(rightSide)
+      let leftIndex = 0, rightIndex = 0, globalIndex = 0
+      while (leftIndex < leftSide.length && rightIndex < rightSide.length) {
+        setTimeout(() => { }, 100000);
+        if (leftSide[leftIndex] < rightSide[rightIndex]) {
+          tab[globalIndex] = leftSide[leftIndex];
+          leftIndex++;
+        } else {
+          tab[globalIndex] = rightSide[rightIndex];
+          rightIndex++;
+        }
+        globalIndex++;
+      }
+      while (leftIndex < leftSide.length) {
+        setTimeout(() => { }, 100000);
+        tab[globalIndex] = leftSide[leftIndex];
+        leftIndex++;
+        globalIndex++;
+      }
+      while (rightIndex < rightSide.length) {
+        setTimeout(() => { }, 100000);
+        tab[globalIndex] = rightSide[rightIndex];
+        rightIndex++;
+        globalIndex++;
+      }
+    }
+    this.tab = tab;
+  }
+
+  //Algorithme de tri rapide
+  public sortQuick(tab) {
+    if (tab.length < 2) {
+      return tab;
+    }
+
+    let pivot = tab[0];
+    let lesserArray = [];
+    let greaterArray = [];
+
+    for (let i = 1; i < tab.length; i++) {
+      setTimeout(() => { }, 100000);
+      if (tab[i] > pivot) {
+        greaterArray.push(tab[i]);
+      } else {
+        lesserArray.push(tab[i]);
+      }
+    }
+
+    return this.sortQuick(lesserArray).concat(pivot, this.sortQuick(greaterArray));
+  }
+
+  //Appel de l'algo de fusion pour chrono
+  public callSortFusion(tab) {
+    let start = performance.now();
+    console.log(start);
+
+    this.sortFusion(tab);
+
+    let end = performance.now();
+    console.log(end);
+    this.time = end - start;
+  }
+
+  //Appel de l'algo de rapide pour chrono
+  public callSortQuick(tab) {
+    let start = performance.now();
+    console.log(start);
+
+    this.tab = this.sortQuick(tab);
+
     let end = performance.now();
     console.log(end);
     this.time = end - start;
